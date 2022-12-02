0fc5ae013fc246389e0324ed312d908c2f53d749
Alexander <sany199574@gmail.com>
2022-12-02 22:09:09 +0300

    Добавлен абстрактный класс для DTO. Который будет контролировать время создания, модификацй, кто создал, кто изменил

---
+++ PS.ApplicationCQRS/PS.LeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace PS.LeaveManagement.Domain.Common
+{
+    abstract class BaseDomainEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime ModifidedDate { get; set; }
+        public string? ModifidedBy { get; set; }
+    }
+}
