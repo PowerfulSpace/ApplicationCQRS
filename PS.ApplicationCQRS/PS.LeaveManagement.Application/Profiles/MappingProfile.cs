c715484ab83e76223c238f0ac7a070c79a0d534b
Alexander <sany199574@gmail.com>
2022-12-06 22:44:33 +0300

    Подключили маппинг

---
+++ PS.ApplicationCQRS/PS.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using PS.LeaveManagement.Application.DTOs.LeaveAllocation;
+using PS.LeaveManagement.Application.DTOs.LeaveRequest;
+using PS.LeaveManagement.Application.DTOs.LeaveType;
+using PS.LeaveManagement.Domain;
+
+namespace PS.LeaveManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            #region LeaveType Mappings
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
+            #endregion
+
+            #region LeaveRequest Mappings
+            CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+            #endregion
+
+            #region LeaveAllocation Mappings
+            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+            #endregion
+        }
+    }
+}
