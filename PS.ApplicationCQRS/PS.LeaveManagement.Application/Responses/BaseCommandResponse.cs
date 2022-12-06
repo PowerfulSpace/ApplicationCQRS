fcc457fc06b049ffca3736d021b2c6cf12022a47
Alexander <sany199574@gmail.com>
2022-12-06 22:49:55 +0300

    Добавлена базов комманда ответа

---
+++ PS.ApplicationCQRS/PS.LeaveManagement.Application/Responses/BaseCommandResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace PS.LeaveManagement.Application.Responses
+{
+    public class BaseCommandResponse
+    {
+        public int Id { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; } = null!;
+        public List<string>? Errors { get; set; }
+    }
+}
